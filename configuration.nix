6e426435ab31b38d5e7a504f8c52d70df47100ac
Erik Johansson <erik.johansson89@gmail.com>
2024-11-23 21:49:10 +0100

    add disko

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
+  #boot.loader.grub.efiInstallAsRemovable = true;
   services.sshd.enable = true;
   # Setup keyfile
   networking.hostName = "nixos"; # Define your hostname.
